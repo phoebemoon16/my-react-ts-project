9c4618e403d6fb4586d60884f4356b17abe81bae
wanghh5 <wanghh5@baoneng.com>
2023-12-12 11:02:48 +0800

    提交多组件table过滤

--- src/pages/Sortable/index.tsx
+++ src/pages/Sortable/index.tsx
@@ -1,4 +1,4 @@
-
+import { useState }  from 'react'
 function ProductCategoryRow({category}){
   return (
     <tr>
@@ -8,11 +8,11 @@ function ProductCategoryRow({category}){
     </tr>
   )
 }
-function ProductRow({product}){
-  const name = product.stocked ? product.name: 
+function ProductRow({ product }){
+  const name: string | Element = product.stocked ? product.name: 
   <span style={{ color: 'red' }}>
-  {product.name}
-</span>;
+      {product.name}
+  </span>;
 
   return (
     <tr>
@@ -22,11 +22,18 @@ function ProductRow({product}){
   )
 }
 
-function ProjecTable({ products }) {
-  const rows = []
-  let lastCategory = null;
+function ProjecTable({ products, filterText, inStockOnly }) {
+  const rows: [] = []
+  let lastCategory: null = null;
 
-  products.forEach(product:any => {
+  products.forEach(product: ProductType => {
+    // 将name中不含过滤字符的字段 过滤掉
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return
+    }
+    if(inStockOnly && !product.stocked) {
+      return
+    }
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow
@@ -39,6 +46,7 @@ function ProjecTable({ products }) {
         category={product}
         key={product.name} />
     );
+    lastCategory = product.category;
   })
   return (
     <table>
@@ -53,12 +61,17 @@ function ProjecTable({ products }) {
   )
 }
 
-function SearchBar() {
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange}) {
   return (
     <form>
-      <input type="text" placeholder="Search ..."></input>
+      <input type="text" value={{filterText}} placeholder="Search ..."  onChange={(e) => onFilterTextChange(e.target.value)} ></input>
       <label>
-        <input type="checkbox"></input> only show products in stock
+        <input type="checkbox" checked = {inStockOnly}  onChange={(e) => onInStockOnlyChange(e.target.checked)} ></input> 
+        only show products in stock
       </label>
     </form>
   )
@@ -66,14 +79,25 @@ function SearchBar() {
 
 // 过滤组件table
 function FilterProductTable({ products }) {
+
+  const [filterText, setFilterText] = useState('')
+  const [inStockOnly, setInstockOnly] = useState(false)
+
   return (
     <div>
-      <SearchBar></SearchBar>
-      <ProjecTable products={products}></ProjecTable>
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly} onFilterTextChange={setFilterText} onInStockOnlyChange={setInstockOnly}></SearchBar>
+      <ProjecTable products={products} filterText={filterText} inStockOnly={inStockOnly}></ProjecTable>
     </div>
   )
 }
-const PRODUCTS: any = [
+
+interface ProductType {
+  category: string,
+  price: string,
+  stocked: boolean,
+  name: string
+}
+const PRODUCTS: ProductType[] = [
   { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
   { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
   { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
